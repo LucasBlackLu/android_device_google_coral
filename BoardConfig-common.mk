0032d6f76496ef6b8ce389d84132e9867c0ede0e
PixelBoot <bootingpixels@gmail.com>
2022-01-23 15:32:09 +0100

    floral: inherit proper lld definitions

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -40,13 +40,13 @@ TARGET_BOARD_COMMON_PATH := device/google/coral/sm8150
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
-KERNEL_LD := LD=ld.lld
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
+KERNEL_SUPPORTS_LLVM_TOOLS := true
 TARGET_KERNEL_CONFIG := floral_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
