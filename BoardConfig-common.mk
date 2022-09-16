b1915460266edee3d87c062153fa394429d29671
Chirayu Desai <chirayudesai1@gmail.com>
2022-09-17 04:47:50 +0530

    Enable GRALLOC_HANDLE_HAS_RESERVED_SIZE
    * Need to make camera work on 13
    
    Change-Id: I0d78f44386129b1fad30bec6ef82000df130d631

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -238,6 +238,9 @@ DEVICE_MATRIX_FILE := device/google/coral/compatibility_matrix.xml
 # Intall device framework compatibility matrix to product partition
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE := device/google/coral/device_framework_matrix_product.xml
 
+# Needed for camera
+TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
+
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
 
