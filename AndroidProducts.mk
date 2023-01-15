2e528fec5322c20bbbdf6a19fca861daafa52980
LucasblackLu <lucasblack997@gmail.com>
2023-01-16 02:08:30 +0800

    derp-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -25,11 +25,11 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_flame.mk \
     $(LOCAL_DIR)/aosp_coral_hwasan.mk \
     $(LOCAL_DIR)/aosp_flame_hwasan.mk \
-    $(LOCAL_DIR)/lineage_coral.mk \
-    $(LOCAL_DIR)/lineage_flame.mk
+    $(LOCAL_DIR)/derp_coral.mk \
+    $(LOCAL_DIR)/derp_flame.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_coral-userdebug \
     aosp_flame-userdebug \
-    lineage_coral-userdebug \
-    lineage_flame-userdebug
+    derp_coral-userdebug \
+    derp_flame-userdebug
