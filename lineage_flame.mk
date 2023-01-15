924d181a0b0146806aa834fad016051823e48215
LucasblackLu <lucasblack997@gmail.com>
2023-01-16 00:43:08 +0800

    first commit

---
+++ lineage_flame.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Lineage stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/coral/aosp_flame.mk)
+
+include device/google/coral/flame/device-lineage.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 4
+PRODUCT_NAME := lineage_flame
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2280
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=flame \
+    PRIVATE_BUILD_DESC="flame-user 13 TP1A.221005.002 9012097 release-keys"
+
+BUILD_FINGERPRINT := google/flame/flame:13/TP1A.221005.002/9012097:user/release-keys
+
+$(call inherit-product, vendor/google/flame/flame-vendor.mk)
