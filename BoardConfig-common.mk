1e0a2022a012b00eb36ee522a1ac59a45a551e4a
aswin7469 <aswinas@pixysos.com>
2023-10-10 13:06:31 +0530

    floral : BUILD_BROKEN_CLANG_PROPERTY
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -19,6 +19,7 @@ include build/make/target/board/BoardConfigPixelCommon.mk
 
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_CLANG_PROPERTY := true
 
 TARGET_BOARD_PLATFORM := msmnile
 TARGET_BOARD_INFO_FILE := device/google/coral/board-info.txt
