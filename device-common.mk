LineageOS/android_device_google_sunfish

Migrate I/O scheduler parameters to task_profiles.json
This patch preserves the CFQ I/O scheduler parameters:
* Foreground blkio.weight     (CfqWeight)    1000
* Foreground blkio.group_idle (CfqGroupIdle) 2000
* Background blkio.weight     (CfqWeight)     200
* Background blkio.group_idle (CfqGroupIdle)    0

This patch prepares for migration of the blkio controller to the v2
cgroup hierarchy.

Bug: 213617178
Change-Id: I868e766092d875813a5f5e448631d59c38915649
Signed-off-by: Bart Van Assche <bvanassche@google.com>

device-common.mk
@@ -78,6 +78,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     device/google/sunfish/init.logging.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_PLATFORM).logging.rc
 
+PRODUCT_COPY_FILES += \
+    device/google/sunfish/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Pixelstats broken mic detection
 PRODUCT_PROPERTY_OVERRIDES += vendor.audio.mic_break=true
 

LucasBlackLu/android_device_google_coral

device-common.mk
@@ -78,6 +78,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     device/google/coral/init.logging.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_PLATFORM).logging.rc
 
+PRODUCT_COPY_FILES += \
+    device/google/coral/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Pixelstats broken mic detection
 PRODUCT_PROPERTY_OVERRIDES += vendor.audio.mic_break=true
 